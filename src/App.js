2c4b1eafb4d31672513c2e2f6fec285adc146f7b
TopboySaint <topboysaint1@gmail.com>
2025-08-11 14:37:53 +0100

    Update route path for Todo component to root

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
     <Routes>
-     <Route path='/todo' element={<Todo/>}></Route>
+     <Route path='/' element={<Todo/>}></Route>
     </Routes>
       
   )
